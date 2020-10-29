3b367fd0ad74bc2a7771509adffc0eb3054c82d7
AnidemDex <dav.medrin@gmail.com>
2020-10-29 15:45:00 -0500

    Nuevo menu
    Creacion de un nuevo diseño para el menu. Aqui solo se ha creado el item generico, aun faltan piezas

---
+++ src/Scripts/menu_item_title.gd
@@ -0,0 +1,28 @@
+tool
+extends Control
+
+export(String) var title = "----Titulo----" setget set_title
+var offset = Vector2(32,32)
+
+func _ready():
+	rect_size = $Title.rect_size+offset
+
+
+func _process(_delta):
+	if Engine.editor_hint:
+		rect_size = $Title.rect_size+offset
+
+func set_title(new_title):
+	title = new_title
+	property_list_changed_notify()
+	
+	if not is_inside_tree() || not Engine.editor_hint:
+		return
+	
+	if title != null:
+		$Title.set_text(title)
+		property_list_changed_notify()
+	
+	rect_size = $Title.rect_size+offset
+	property_list_changed_notify()
+	pass
