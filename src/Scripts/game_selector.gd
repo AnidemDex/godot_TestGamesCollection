a29c090a7867e1e1e4968d2a2c4baefa0aaed3be
Dex <dav.medrin@gmail.com>
2020-10-30 00:19:13 -0500

    Menu derecho añadido.
    Probablemente esto sea volatil

--- src/Scripts/game_selector.gd
+++ src/Scripts/game_selector.gd
@@ -1,11 +1,14 @@
 extends Control
 
-var game_index = 0
+signal item_started_out
+signal item_finished_in
 
+var game_index = 0
 
 onready var game: Array = [
 	$CircleEvaderSelector,
 	$RhythmHeroSelector,
+	$FallGameSelector,
 	]
 	
 onready var tween = $Tween
@@ -16,6 +19,7 @@ func _ready():
 		item.margin_right = 0
 		item.margin_top = 260
 		item.margin_bottom = -441
+		item.connect("next_button_pressed", self, "change_to_next_selector")
 	
 	put_first_selector()
 	
@@ -24,12 +28,15 @@ func put_first_selector():
 	tween.interpolate_property(game[0], "margin_top", null, 0, 1, Tween.TRANS_ELASTIC, Tween.EASE_IN_OUT)
 	tween.interpolate_property(game[0], "margin_bottom", null, 181, 1, Tween.TRANS_ELASTIC, Tween.EASE_IN_OUT)
 	tween.start()
+	yield(tween, "tween_all_completed")
+	emit_signal("item_finished_in")
 
 func change_to_next_selector():
+	emit_signal("item_started_out")
 	tween.interpolate_property(game[game_index], "margin_left", null, -300, 1, Tween.TRANS_ELASTIC, Tween.EASE_IN_OUT)
 	tween.interpolate_property(game[game_index], "margin_right", null, -240, 1, Tween.TRANS_ELASTIC, Tween.EASE_IN_OUT)
 	tween.start()
-	yield(get_tree().create_timer(1.1), "timeout")
+	yield(tween, "tween_all_completed")
 	
 	game[game_index].margin_left = -139
 	game[game_index].margin_right = 0
@@ -40,12 +47,6 @@ func change_to_next_selector():
 	tween.interpolate_property(game[game_index], "margin_top", null, 0, 1, Tween.TRANS_ELASTIC, Tween.EASE_IN_OUT)
 	tween.interpolate_property(game[game_index], "margin_bottom", null, 181, 1, Tween.TRANS_ELASTIC, Tween.EASE_IN_OUT)
 	tween.start()
+	yield(tween, "tween_all_completed")
+	emit_signal("item_finished_in")
 	
-
-
-func _on_RhythmHeroSelector_next_button_pressed():
-	change_to_next_selector()
-
-
-func _on_CircleEvaderSelector_next_button_pressed():
-	change_to_next_selector()
