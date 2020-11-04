1cb7070b4ad0d461c470ce08cc85f58ba7100885
Dex <dav.medrin@gmail.com>
2020-11-04 00:23:41 -0500

    Mejora del algoritmo procedural.
    Mira, lector, seré honesto contigo. No tengo idea de como lo estoy haciendo, asi que no uses este commit

--- src/caida_procedural/scripts/level_generator.gd
+++ src/caida_procedural/scripts/level_generator.gd
@@ -1,78 +1,33 @@
+tool
 extends Node2D
-class_name LevelGenerator
-# Reglas al generar
-# 1. Debe tener paredes laterales X[0:11].
-# 2. Debe tener una salida de 3 casillas.
-#    a) La salida mide 2 casillas de alto
-#    b) Las casillas laterales tienen estructura solida
-#       hasta llegar a la pared solamente en la casilla
-#       superior.
 
-signal started
-signal finished
+export(float, -0.1, 0.5, 0.1) var test_range = 0.5
 
-enum Cell {
-	DESTRUCTUBLE, 
-	GROUND, 
-	OUTER
-	}
-
-var _random_number = RandomNumberGenerator.new()
-var size = Vector2.ZERO
-var start_point = Vector2.ZERO
-
-onready var _tilemap : TileMap = $Enviorment
-onready var reference_size = $ReferenceSize
+export(Vector2) var start_point = Vector2.ZERO
+export(Vector2) var end_point = Vector2(64,64)
+export(OpenSimplexNoise) var simple_noise
 
 func _ready():
-	_random_number.randomize()
-	setup()
-	print("start: ", start_point, " end: ", size)
-	generate()
+	$SolidEnviorment.clear()
+	set_process(false)
+	randomize()
+	_check_simple_noise()
+	generate_solid_enviorment()
 
-func setup():
-	size = (reference_size.rect_size+reference_size.rect_global_position)/16
-	size = Vector2(int(size.x), int(size.y))
-	start_point = reference_size.rect_global_position/16
-	start_point = Vector2(int(start_point.x), int(start_point.y))
+func generate_solid_enviorment():
+	for x in range(start_point.x, end_point.x):
+		for y in range(start_point.y, end_point.y):
+			var a = simple_noise.get_noise_2d(x, y)
+			if a > test_range:
+				$SolidEnviorment.set_cell(x,y,0)
+	
+	$SolidEnviorment.update_bitmask_region(Vector2.ZERO, end_point)
 	pass
 
-func generate():
-	emit_signal("started")
-	_generate_perimeter()
-	_generate_inner()
-	emit_signal("finished")
-	pass
-
-func _generate_perimeter():
-	# Generar las paredes laterales
-	for y in range(0, size.y):
-		_tilemap.set_cell(start_point.x, y, 1)
-		_tilemap.set_cell(size.x, y, 4)
-	
-	# Generar salida
-	for x in range(0, size.x):
-		match x:
-			3:
-				_tilemap.set_cell(start_point.x+x, size.y, 5, true)
-			6:
-				_tilemap.set_cell(start_point.x+x, size.y, 5)
-			_:
-				_tilemap.set_cell(start_point.x+x, size.y, 0)
-				
-	_tilemap.set_cell(start_point.x+0, size.y-1, 0)
-	_tilemap.set_cell(start_point.x+1, size.y-1, 3, true)
-	_tilemap.set_cell(start_point.x+2, size.y-1, 3, true)
-	_tilemap.set_cell(start_point.x+3, size.y-1, 2, true)
-	
-	_tilemap.set_cell(start_point.x+4, size.y-1, 0)
-	_tilemap.set_cell(start_point.x+5, size.y-1, 0)
-	
-	_tilemap.set_cell(start_point.x+6, size.y-1, 2)
-	_tilemap.set_cell(start_point.x+7, size.y-1, 3)
-	_tilemap.set_cell(start_point.x+8, size.y-1, 3)
-	_tilemap.set_cell(start_point.x+9, size.y-1, 0)
-	
-
-func _generate_inner():
-	pass
+func _check_simple_noise():
+	if simple_noise == null:
+		simple_noise = OpenSimplexNoise.new()
+		simple_noise.octaves = 1.0
+		simple_noise.period = 12
+		simple_noise.persistence = 0.7
+	simple_noise.seed = randi()
